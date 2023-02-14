439d073a03988de5d71bed4894573b5a3f3cfe3c
Maurice Zhou <jasper@apvc.uk>
2023-02-14 21:25:41 +0100

    fix installation script
    Signed-off-by: Maurice Zhou <jasper@apvc.uk>

--- .config/nixos/inst.sh
+++ .config/nixos/inst.sh
@@ -10,14 +10,76 @@ tee -a dotfiles-nixos/.config/nixos/machine.nix <<EOF
 }
 EOF
 
-for i in ${DISK}; do blkdiscard -f $i sgdisk --zap-all $i sgdisk -n1:1M:+1G -t1:EF00 $i sgdisk -n2:0:+4G -t2:BE00 $i sgdisk -n4:0:+${INST_PARTSIZE_SWAP}G -t4:8200 $i if test -z $INST_PARTSIZE_RPOOL; then     sgdisk -n3:0:0   -t3:BF00 $i; else     sgdisk -n3:0:+${INST_PARTSIZE_RPOOL}G -t3:BF00 $i; fi sgdisk -a1 -n5:24K:+1000K -t5:EF02 $i sync && udevadm settle && sleep 3; cryptsetup open --type plain --key-file /dev/random $i-part4 ${i##*/}-part4; mkswap /dev/mapper/${i##*/}-part4; swapon /dev/mapper/${i##*/}-part4; done
-zpool create       -o compatibility=grub2       -o ashift=12       -o autotrim=on       -O acltype=posixacl       -O canmount=off       -O compression=lz4       -O devices=off       -O normalization=formD       -O atime=off       -O xattr=sa       -O mountpoint=/boot       -R /mnt     bpool       $(for i in ${DISK}; do
+for i in ${DISK}; do
+blkdiscard -f $i
+sync
+udevadm settle
+
+sgdisk --zap-all $i
+
+sgdisk -n1:1M:+1G -t1:EF00 $i
+
+sgdisk -n2:0:+4G -t2:BE00 $i
+
+sgdisk -n4:0:+${INST_PARTSIZE_SWAP}G -t4:8200 $i
+
+if test -z $INST_PARTSIZE_RPOOL; then
+    sgdisk -n3:0:0   -t3:BF00 $i
+else
+    sgdisk -n3:0:+${INST_PARTSIZE_RPOOL}G -t3:BF00 $i
+fi
+
+sgdisk -a1 -n5:24K:+1000K -t5:EF02 $i
+
+sync && udevadm settle && sleep 3
+
+cryptsetup open --type plain --key-file /dev/random $i-part4 ${i##*/}-part4
+mkswap /dev/mapper/${i##*/}-part4
+swapon /dev/mapper/${i##*/}-part4
+done
+
+zpool create \
+      -o compatibility=grub2 \
+      -o ashift=12 \
+      -o autotrim=on \
+      -O acltype=posixacl \
+      -O canmount=off \
+      -O compression=lz4 \
+      -O devices=off \
+      -O normalization=formD \
+      -O atime=off \
+      -O xattr=sa \
+      -O mountpoint=/boot \
+      -R /mnt     bpool \
+      $(for i in ${DISK}; do
 	    printf "$i-part2 ";
 	done)
-zpool create       -o ashift=12       -o autotrim=on       -R /mnt        -O  acltype=posixacl       -O canmount=off       -O compression=zstd       -O dnodesize=auto       -O normalization=formD       -O atime=off       -O xattr=sa       -O mountpoint=/       rpool       $(for i in ${DISK}; do
+zpool create \
+      -o ashift=12 \
+      -o autotrim=on \
+      -R /mnt  \
+      -O  acltype=posixacl \
+      -O canmount=off \
+      -O compression=zstd \
+      -O dnodesize=auto \
+      -O normalization=formD \
+      -O atime=off \
+      -O xattr=sa \
+      -O mountpoint=/ \
+      rpool \
+      $(for i in ${DISK}; do
       printf "$i-part3 ";
      done)
-echo poolpass |     zfs create 	-o canmount=off 	-o mountpoint=none 	-o encryption=on 	-o keylocation=prompt 	-o keyformat=passphrase 	rpool/nixos
+
+echo poolpass | \
+    zfs create \
+	-o canmount=off \
+	-o mountpoint=none \
+	-o encryption=on \
+	-o keylocation=prompt \
+	-o keyformat=passphrase \
+	rpool/nixos
+
 zfs create -o mountpoint=legacy     rpool/nixos/root
 mount -t zfs rpool/nixos/root /mnt/
 zfs create -o mountpoint=legacy rpool/nixos/home
@@ -32,8 +94,19 @@ mkdir /mnt/boot
 mount -t zfs bpool/nixos/root /mnt/boot
 zfs create -o mountpoint=legacy rpool/nixos/empty
 zfs snapshot rpool/nixos/empty@start
-for i in ${DISK}; do     mkfs.vfat -n EFI ${i}-part1;     mkdir -p /mnt/boot/efis/${i##*/}-part1;     mount -t vfat ${i}-part1 /mnt/boot/efis/${i##*/}-part1; done
+
+for i in ${DISK}; do
+    mkfs.vfat -n EFI ${i}-part1;
+    mkdir -p /mnt/boot/efis/${i##*/}-part1;
+    mount -t vfat ${i}-part1 /mnt/boot/efis/${i##*/}-part1;
+done
+
 mv dotfiles-nixos /mnt/home/user
 cp -r /mnt/home/user/.config/nixos/ /mnt/etc/
 chown -R 1001:100 /mnt/home/user/
 history -w /mnt/home/user/.config/nixos/inst.sh
+nixos-install --no-root-passwd --root /mnt
+#history -w /mnt/home/inst.sh
+
+umount -Rl /mnt
+zpool export -a
